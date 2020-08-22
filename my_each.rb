dbeac181d89f9c6d84601d3a750f4d0101c9d008
Brenda Ferrufino <ferrufinob@gmail.com>
2020-08-22 22:00:08 +0000

    Done.

--- my_each.rb
+++ my_each.rb
@@ -1,3 +1,9 @@
-def my_each # put argument(s) here
+def my_each(element) # put argument(s) here
   # code here
+  i = 0
+  while i < element.length 
+  yield(element[i])
+   i = i + 1
+end
+element
 end
