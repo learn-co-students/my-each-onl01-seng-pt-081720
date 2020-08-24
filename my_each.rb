771be5bdb031f25b89da79680f017bdb76b4a423
Jazmin Matos <jmatos1993@gmail.com>
2020-08-24 00:38:29 +0000

    Done.

--- my_each.rb
+++ my_each.rb
@@ -1,3 +1,13 @@
-def my_each # put argument(s) here
-  # code here
+def my_each(array)
+    i = 0
+    
+    while i < array.length
+      yield array[i]
+      i += 1
+    end
+    array
 end
+
+# accept an argument of an array
+# use the while loop to iterate over each member of that array
+# yield each element contained in the array to a block
