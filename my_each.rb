35405f7b8f23fec252ed2a9f5a2fabf8c83e7438
Myles Janik <mylesjanik@gmail.com>
2020-08-30 23:54:10 +0000

    Automatically backed up by Learn

--- my_each.rb
+++ my_each.rb
@@ -2,7 +2,7 @@ def my_each(array) # put argument(s) here
   # code here
   counter = 0
   while array.length > counter
-    yield puts array[counter]
+    yield array[counter]
     counter += 1
   end
   array
